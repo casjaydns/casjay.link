faae7323c73c4122cf6e4394097869e6e1dfe10c
casjay <git-admin@casjaysdev.com>
2021-08-25 18:10:49 -0400

    Create navBar.js

---
+++ public/js/navBar.js
@@ -0,0 +1,10 @@
+<script>
+function navBar() {
+  var x = document.getElementById("navBarLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+</script>
