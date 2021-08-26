59f60260d8f74bb88f8d6193d8480379f2617e00
casjay <git-admin@casjaysdev.com>
2021-08-25 20:36:19 -0400

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -23,7 +23,7 @@ const app = new Vue({
       if (response.ok) {
         const result = await response.json();
         this.formVisible = false;
-        this.created = `//${location.host}/${result.slug}`;
+        this.created = `https://casjay.link/${result.slug}`;
       } else if (response.status === 429) {
         this.error = 'You are sending too many requests. Try again in 30 seconds.';
       } else {
